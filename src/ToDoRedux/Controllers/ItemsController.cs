a9fecbeecb6b199cf2a83f03e5be0f038666a59e
Kaz Matthews and Sam Kirsch <kaz+sam@>
2017-04-17 10:13:07 -0700

    Make base directories and add files and complete portions of routing

---
+++ src/ToDoRedux/Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoRedux.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoRedux.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoReduxContext db = new ToDoReduxContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/ToDoRedux/Models/ToDoReduxContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoRedux.Models
+{
+    public class ToDoReduxContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ToDoRedux;integrated security=True");
+        }
+    }
+}
